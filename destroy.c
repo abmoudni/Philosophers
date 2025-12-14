cae4d2c9e0ebb36a920eb930444d63beaa91ce78
abmoudni <abdsamadmoudnib@gmail.com>
2025-12-14 05:18:40 +0100

    FIX: OPAQUE DATA TYPES (PTHREAD)

--- destroy.c
+++ destroy.c
@@ -8,16 +8,14 @@ void	destroy_params(t_params *params)
 	if (!params->forks)
 		return ;
 	i = 0;
-	// while (i < params->num_philos && params->forks[i] != NULL)
-	// {
-	// 	pthread_mutex_destroy(&params->forks[i]);
-	// 	i++;
-	// }
-	// free(params->forks);
-	// if (params->someone_died_mtx)
-	// 	pthread_mutex_destroy(&params->someone_died_mtx);
-	// if (params->print_lock)
-	// 	pthread_mutex_destroy(&params->print_lock);
+	while (i < params->num_philos)
+	{
+		pthread_mutex_destroy(&params->forks[i]);
+		i++;
+	}
+	free(params->forks);
+	pthread_mutex_destroy(&params->someone_died_mtx);
+	pthread_mutex_destroy(&params->print_lock);
 }
 
 void	destroy_philos(t_philo *philos, int num_philos)
@@ -31,10 +29,8 @@ void	destroy_philos(t_philo *philos, int num_philos)
 	while (i < num_philos)
 	{
 		p = &philos[i];
-		// if (p->meals_eat_mtx)
-		// 	pthread_mutex_destroy(&p->meals_eat_mtx);
-		// if (p->last_meal_time_mtx)
-		// 	pthread_mutex_destroy(&p->last_meal_time_mtx);
+		pthread_mutex_destroy(&p->meals_eat_mtx);
+		pthread_mutex_destroy(&p->last_meal_time_mtx);
 		i++;
 	}
 	free(philos);
